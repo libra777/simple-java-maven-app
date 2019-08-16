5e12b078d88f8c911135365667583be6389f4c2f
qipengsun <qipeng.sun@gmail.com>
2019-08-16 16:47:33 +0800

    ok

---
+++ src/main/java/com/mycompany/app/Address.java
@@ -0,0 +1,13 @@
+package com.mycompany.app;
+
+import lombok.Data;
+
+/**
+ * @sunqipeng --@2019-08-16-16:44
+ **/
+@Data
+
+public class Address {
+    private String name;
+
+}
--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,14 +1,21 @@
 package com.mycompany.app;
 
+import lombok.extern.java.Log;
+
 /**
  * Hello world!
  */
+@Log
 public class App {
 
     private final String message = "Hello World!";
 
     public App() {
+        Address address = new Address();
+        address.setName("ok");
+        System.out.println(address);
         System.out.println("the init app class");
+        log.info("some information");
     }
 
     public static void main(String[] args) {
